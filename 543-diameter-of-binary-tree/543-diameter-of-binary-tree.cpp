45d44b403582e3f73e888130d530a79ec2bc1d90
Aditi1220-kinjal <103524957+Aditi1220-kinjal@users.noreply.github.com>
2022-08-04 19:43:07 +0530

    Time: 41 ms (5.26%), Space: 23.4 MB (7.49%) - LeetHub

---
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans=0;
+    unordered_map<TreeNode*,int> mp; // stores max path length starting from that node
+    int dfs(TreeNode* node){
+        if(!node) // nullptr node
+            return 0;
+        if(!mp.count(node)){
+            pair<int,int>p={1+dfs(node->left),1+dfs(node->right)};
+            mp[node]=max(p.first,p.second);  // max path
+            ans = max(ans,p.first+p.second-2); // -1 since we are counting the current node twice 
+            // and another -1 because path length consisting of n nodes is n-1
+        }
+        return mp[node];
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
